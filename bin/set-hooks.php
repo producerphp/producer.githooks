bbe085fd928889d09f8a05679a72bf019ad31100
Paul M. Jones <pmjones88@gmail.com>
2019-04-23 14:06:30 -0500

    require the class file

--- bin/set-hooks.php
+++ bin/set-hooks.php
@@ -1,4 +1,5 @@
 <?php
+require dirname(__DIR__) . '/src/SetHooks.php';
 $dir = $_SERVER['argv'][1] ?? dirname(dirname(dirname(dirname(__DIR__))));
 $dir = realpath($dir);
 $command = new \Producer\Githooks\SetHooks();
